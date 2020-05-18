5d4b4ec994f258ae1af580d7c13d16d0e9dd20fa
zubquzaini <zubliquzaini@gmail.com>
2020-05-18 13:40:51 +0800

    Example 7

---
+++ Blended Learning 9/Example 7/Stack.java
@@ -0,0 +1,19 @@
+public class Stack extends LinkList {
+    
+    public Stack() {
+    }
+    
+    public void push(Object elem) {
+        
+        insertAtFront(elem);
+    }
+    
+    public Object pop() {
+        return removeFromFront();
+    }
+    
+    public Object peek() {
+        
+        return getFirst();
+    }
+}

dd268e6a2a98858ea7b3048628c22f7daef2872b
zubquzaini <zubliquzaini@gmail.com>
2020-05-18 14:25:16 +0800

    Example 8

---
+++ Blended Learning 9/Example 7/Employee.java
@@ -0,0 +1,42 @@
+public class Employee {
+
+    
+    private String employID;
+    private String empName;
+    private int eAge;
+    private double salary;
+
+    public Employee(String employID, String empName, int eAge, double salary) {
+        
+        this.employID=employID;
+        this.empName=empName;
+        this.eAge = eAge;
+        this.salary = salary;
+    }
+   
+    public String getID() {
+        
+        return employID;
+    }
+    
+    public String getName() {
+        
+        return empName;
+    }
+    
+    public int getAge() {
+        
+        return eAge;
+    }
+    
+    public double getSalary() {
+        
+        return salary;
+    }
+
+    
+    public String toString() {
+        
+        return ("\nID: " + employID + "\nName: " + empName);
+    }
+}
--- Blended Learning 9/Example 7/StackApp.java
+++ Blended Learning 9/Example 7/StackApp.java
@@ -4,39 +4,76 @@ public class StackApp {
     
     public static void main(String args[]) {
         System.out.println();
-        Stack myStack = new Stack();
+        Stack theStack = new Stack();
         Stack tempStack = new Stack();
-        String num;
         
-        // push data into stack
-        for(int i=0; i<3; i++) {
+        for(int i=0; i<5; i++) {
             
-            num = JOptionPane.showInputDialog("Enter a number");
-            myStack.push(num);
+            String eId = JOptionPane.showInputDialog("Enter employee id");
+            String eName = JOptionPane.showInputDialog("Enter employee name");
+            String eAge = JOptionPane.showInputDialog("Enter age");
+            String eSalary = JOptionPane.showInputDialog("Enter salary");
+            int age = Integer.parseInt(eAge);
+            double salary = Double.parseDouble(eSalary);
+            Employee emp = new Employee(eId, eName, age, salary);
+            theStack.push(emp);
         }
         
-        int count=0, sum=0, min=9999, max=-9999;
+        // to display all the employees in the stack
+        Object data;
+        Employee E;
         
-        while(!myStack.isEmpty()) {
+        while(!theStack.isEmpty()) {
             
-            Object data = myStack.pop();
-            count++;
-            int no = Integer.parseInt(data.toString());
-            sum = sum + no;
-            if(no > max) {
-                max = no;
-            }
-            
-            if(no < min) {
-                min = no;
-            }
-            tempStack.push(data);
+            data = theStack.pop();
+            E = (Employee) data;
+            System.out.println(E.toString());
+            tempStack.push(E);
         }
-        double average = sum/count;
         
-        JOptionPane.showMessageDialog(null, "There are " + count + " items in the stack");
-        JOptionPane.showMessageDialog(null, "The sum of all items in the stack is " + sum);
-        JOptionPane.showMessageDialog(null, "The minimum is " + min);
-        JOptionPane.showMessageDialog(null, "The maximum is " + max);
+        // to display all the data from temporary stack into original stack
+        while(!tempStack.isEmpty()) {
+            
+            data = tempStack.pop();
+            E = (Employee) data;
+            theStack.push(E);
+        }
+        
+        int numAge=0, num2000=0;
+        while(!theStack.isEmpty()) {
+            
+            data = theStack.pop();
+            
+            E = (Employee) data;
+            if(E.getAge() > 40) {
+                
+                numAge++;
+            }
+            
+            if(E.getSalary() > 2000) {
+                
+                num2000++;
+            }
+        }
+        
+        System.out.println("The number of employee that their age is above 40 = " + numAge);
+        System.out.println("The number of employee that salary > 2000 = " + num2000);
+        
+        double maxSalary = -9999.99, minSalary = 9999.99;
+        while(!tempStack.isEmpty()) {
+            
+            data = tempStack.pop();
+            E = (Employee) data;
+            
+            if(E.getSalary() > maxSalary) {
+                maxSalary = E.getSalary();
+            } else {
+                minSalary = E.getSalary();
+            }
+            
+            theStack.push(E);
+            System.out.println("The maximum salary =" + maxSalary);
+            System.out.println("The minimum salary = " + minSalary);
+        }
     }
 }
